ad5fba3665c65303033b5d7e378d1bef0835c36d
Emir(han) Akyol <87340673+EmirAkyoll@users.noreply.github.com>
2023-03-25 21:48:16 +0300

    turn to nextjs

--- src/store/actions/movieActions.js
+++
@@ -1,9 +0,0 @@
-import { ADD_MOVIE } from "../actionTypes/movieActionTypes"
-
-export const addNewMovie = (movie_data) => {
-    console.log("ACTION ÇALIŞTI");
-    return {
-        type: ADD_MOVIE,
-        payload: movie_data
-    }
-}
--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import movieReducer from "./reducers/movieReducer";
-
-const rootReducer = combineReducers({
-    movies: movieReducer,
-})
-
-export default rootReducer;
