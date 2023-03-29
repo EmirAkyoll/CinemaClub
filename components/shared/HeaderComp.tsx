f320c30581b733cb169abf6989ee3f68dce61f41
Emir(han) Akyol <87340673+EmirAkyoll@users.noreply.github.com>
2023-03-29 23:41:09 +0300

    basic configuration II

---
+++ components/shared/HeaderComp.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderComp = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HeaderComp;
---
+++ components/shared/Navigatior.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navigatior = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navigatior
