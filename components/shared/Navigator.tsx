42f6a394ae8f1f322cebac624791f6bcaba508b2
Emir(han) Akyol <87340673+EmirAkyoll@users.noreply.github.com>
2023-04-01 23:28:51 +0300

    project started to be built II

--- components/shared/Navigator.tsx
+++ components/shared/Navigator.tsx
@@ -2,13 +2,20 @@ import React from "react";
 // Styles coming from '_navigator.scss'
 
 const Navigator = () => {
+  function themeToggle() {
+    document.documentElement.style.setProperty('themeMode', 'dark')
+  }
+
   return (
     <nav className="navigation">
       <a href="#" className="navigation-item">Advices</a>
       <a href="#" className="navigation-item">Booked</a>
       <a href="#" className="navigation-item">Nakkara</a>
 
-      <button className="navigation-theme-mode-toggle">
+      <button 
+        className="navigation-theme-mode-toggle" 
+        onClick={() => {themeToggle()} }
+      >
         Light Mode
       </button>
     </nav>
