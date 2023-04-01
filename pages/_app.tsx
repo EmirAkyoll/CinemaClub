d8aafd533f093410292f8793c581d8fef02e4651
Emir(han) Akyol <87340673+EmirAkyoll@users.noreply.github.com>
2023-04-01 23:20:45 +0300

    project started to be built II

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,11 @@
 import type { AppProps } from "next/app";
 import Layout from "../layout/Layout";
 import "../styles/main.scss";
-import { ThemeProvider } from "next-themes";
-
-export let themeMode = "dark";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider 
-      enableSystem={false}
-      defaultTheme="dark" 
-      forcedTheme={themeMode}
-    >
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
